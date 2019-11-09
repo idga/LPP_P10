fbb73e4a69c8c31b14aaf302e2e93bfda27657fc
Isaac Daniel Garcia Armas <alu0100979602@ull.edu.es>
2019-11-09 12:39:22 +0000

    Existe clase Alimento [Falla]

--- spec/prct6_spec.rb
+++ spec/prct6_spec.rb
@@ -1,9 +1,14 @@
 RSpec.describe Prct6 do
+  
   it "has a version number" do
     expect(Prct6::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe Alimento do
+
+	before :each do
+		
+	end
   end
+
 end
