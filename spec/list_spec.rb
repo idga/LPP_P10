8f746b8150ad9e454ee90bb17b2e5023186e0775
Isaac Daniel Garcia Armas <alu0100979602@ull.edu.es>
2019-11-19 13:25:51 +0000

    Inserta [Falla]

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,7 +1,14 @@
 RSpec.describe List do
 	
 	before :each do
-
+		@a1 = Alimento.new("Carne de Vaca",21.1,0.0,3.1,50.0,164.0)
 		@l = List.new()
+	end
+
+	context 'Modificaciones lista' do
+
+		it "Insercion" do
+			expect(@l.insert(@a1)).equal?(Node)
+		end
 	end
 end
